b828b01ae5546924364647738e485827411bca5b
Łukasz Brodny <brodny@wp.pl>
2022-02-23 20:58:10 +0100

    AOPAttribute created

---
+++ LBrodny.AOP/AOPAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+#nullable enable
+
+namespace LBrodny.AOP
+{
+    public abstract class AOPAttribute
+    {
+        public abstract object Execute(
+            MethodInfo method,
+            object[] args,
+            object[] annotations);
+    }
+}
